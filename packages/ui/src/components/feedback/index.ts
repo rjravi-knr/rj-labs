30459edfd8b30c951656e687647fbce9d8d98e66
ravikiran <ravikiran@aceproconsulting.com>
2025-12-03 21:10:01 +0530

    feat: Add `Separator`, `Skeleton`, and `Spinner` UI components, establish main `ui` package exports, and document shared component strategy.

--- packages/ui/src/components/feedback/index.ts
+++ packages/ui/src/components/feedback/index.ts
@@ -1,5 +1,6 @@
-export * from "./alert-dialog"
-export * from "./dialog"
-export * from "./progress"
-export * from "./sheet"
-export * from "./sonner"
+// Feedback
+export * from "./alert-dialog";
+export * from "./dialog";
+export * from "./progress";
+export * from "./sheet";
+export * from "./sonner";
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,10 @@
+// Export all components
+export * from "./components/atoms";
+export * from "./components/molecules";
+export * from "./components/forms";
+export * from "./components/data-display";
+export * from "./components/feedback";
+export * from "./components/navigation";
+
+// Export utils
+export * from "./lib/utils";
