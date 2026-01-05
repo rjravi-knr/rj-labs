0ad253a0f52389ee8657e99b39985ee0c17ea228
ravikiran <ravikiran@aceproconsulting.com>
2026-01-05 22:24:50 +0530

    chore(playground): Update local dev server port to 8000

--- services/playground/src/server.ts
+++ services/playground/src/server.ts
@@ -1,7 +1,7 @@
 import { serve } from '@hono/node-server';
 import app from './app';
 
-const port = 3003;
+const port = 8000;
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
